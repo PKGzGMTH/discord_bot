138332bdf93bab1091504975cebb3ddf2aaecb14
PKGzGMTH <peakungg@gmail.com>
2021-07-18 13:29:52 +0700

    add some chat

--- src/main.py
+++ src/main.py
@@ -14,25 +14,26 @@ async def join(ctx):
     channel = ctx.author.voice.channel
     await channel.connect()
     print('\njoining voice chat ',ctx.author.voice.channel)
-    print(f'cmd : {bot}')
     
 @bot.command(passcontext=True, help = ':Let Me leave the Voice channel')
 async def leave(ctx):
     await ctx.voice_client.disconnect()
     print('leave voice chat',ctx.author.voice.channel)
-    print(f'cmd : {bot}')
 
 @bot.event
 async def on_message(message) :
     # bot.process_commands(msg) is a couroutine that must be called here since we are overriding the on_message event
     await bot.process_commands(message) 
-    if str(message.content).lower() == "hello":
+    if str(message.content).lower().startswith("hello"):
         await message.channel.send('Hi!')
     
-    if str(message.content) == "หิวข้าว":
+    if str(message.content).lower().startswith("paimon"):
+        await message.channel.send('ว่าจะได๋?')
+    
+    if str(message.content).startswith("หิวข้าว"):
         await message.channel.send('จะกินอาหารฉุกเฉินไหมล่ะห้ะ')
     
-    if str(message.content).lower() in ['swear_word1','swear_word2']:
+    if str(message.content).lower() in ['fuck','kuy']:
         await message.channel.purge(limit=1)
 
 
